73ea0bf6207054b30d94de36442fc1ac8345f3ce
Adolfo <adolfoamador@gmail.com>
2023-07-10 22:32:11 -0600

    Registro de Ventas
    Creacion del registro de ventas

---
+++ RegistroVentas.py
@@ -0,0 +1,12 @@
+import csv
+
+producto = input("Producto: ")
+cantidad = input("Cantidad: ")
+precio = input("Precio: ")
+cliente = input("Cliente: ")
+
+with open("RegistroVentas.csv", mode='w') as registro_file:
+    registro_writer = csv.writer(registro_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    registro_writer.writerow([producto, cantidad, precio, cliente])
+    
