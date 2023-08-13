81260c4e9b26157c14079ec756016c2f45d151d9
SebasCordero15 <sebascordero4@gmail.com>
2023-08-13 13:07:47 -0600

    Update Calular IVA.py

--- Calular IVA.py
+++ Calular IVA.py
@@ -1,9 +1,10 @@
 def calcular_precio_con_iva():
     while True:
         precio = float(input("Ingrese el precio (ingrese 0 para salir): "))
+        Valor_IVA = float(input("Ingese el valor de IVA: "))
         if precio == 0:
             break
-        iva = precio * 0.13
+        iva = precio * Valor_IVA
         precio_con_iva = precio + iva
         print(f"El precio con IVA es: {precio_con_iva}")
 calcular_precio_con_iva()
